1d920b4a98698ca63c5a3da9e141c2417b586c4c
chinjuc <chinjul7softtech@gmail.com>
2026-05-18 12:18:05 +0530

    Initial Commit

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
 const tasksRoute = require('./routes/tasks');
