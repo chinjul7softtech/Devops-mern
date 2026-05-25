a63f7f34185bf25e7a5df18ea5fdfdf885c1e8d3
chinjuc <chinjul7softtech@gmail.com>
2026-05-25 13:02:31 +0530

    fix: app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <div style={{ padding: '20px' }}>
-      <h1>To-Do List </h1>
+      <h1>To-Do List Devops</h1>
       <input
         value={text}
         onChange={e => setText(e.target.value)}
